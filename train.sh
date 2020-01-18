051492b6047a65a1b8ad8452488dce9a66c5048f
jinlukang <jlk0617@mail.ustc.edu.cn>
2020-01-18 17:01:46 +0800

    new bert trainable

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 allennlp train -f \
     --include-package tagging \
-    -s /tmp/tagging/DT_output_dirs/test \
-    configs/bert_for_DT_classification.jsonnet
+    -s /tmp/tagging/DT_output_dirs/bert_trainable \
+    configs/bert_trainable.jsonnet
